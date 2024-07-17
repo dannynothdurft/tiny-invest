fd0d89532b271f2f1159a29d0ba2780c50189a94
danny <danny.nothdurft@sugarshape.de>
2024-07-17 08:15:03 +0200

    Start new Projekt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["a.check24.net"],
+  },
+};
 
 export default nextConfig;
