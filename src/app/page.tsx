747d49e0ecc689ae3a5762d6bb3f97f4c04849e2
danny <danny.nothdurft@sugarshape.de>
2024-07-17 12:08:07 +0200

    blog dummy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 import HPConfig from "@/config/homePage.json";
+import BlogCard from "@/components/blogCard";
 
 export default function HomePage() {
   return (
-    <div className="page-main">
-      <section className="section pb-[50px]">
-        <div className="container">
-          <div className="row text-center">
-            <div className="mx-auto lg:col-10">
-              <h1 className="font-primary font-bold">
-                {HPConfig.banner.title}
-              </h1>
-              <p className="mt-4">{HPConfig.banner.content}</p>
-              {HPConfig.banner.button && (
-                <Link
-                  className="btn btn-primary mt-4"
-                  href={HPConfig.banner.button.link}
-                >
-                  {HPConfig.banner.button.label}
-                </Link>
-              )}
-              <Image
-                className="mx-auto mt-12"
-                src={HPConfig.banner.image}
-                width={750}
-                height={390}
-                alt="banner image"
-                priority
-              />
-            </div>
-          </div>
-        </div>
+    <>
+      <section className="section--header">
+        <h1>{HPConfig.banner.title}</h1>
+        <p>{HPConfig.banner.content}</p>
+        <Image
+          src={HPConfig.banner.image}
+          width={750}
+          height={390}
+          alt="banner image"
+          priority
+        />
       </section>
 
-      <section className="flex justify-center">
+      <section className="section">
         <Link
           href="https://a.check24.net/misc/click.php?pid=756943&aid=340&deep=c24bank&cat=14"
           target="_blank"
@@ -48,6 +32,15 @@ export default function HomePage() {
           />
         </Link>
       </section>
-    </div>
+
+      <section className="section--blogs">
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+      </section>
+    </>
   );
 }
---
+++ src/components/blogCard/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+import "@/styles/blogcard.scss";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+function BlogCard() {
+  return (
+    <Link href="#">
+      <div className="blogcard--container">
+        <Image src="/images/banner-art.svg" alt="" width={1000} height={1000} />
+        <div className="p-5">
+          <h5>Noteworthy technology acquisitions 2021</h5>
+          <p>
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order....
+          </p>
+          <div className="card--info">
+            <span>von Danny Nothdurft</span>
+            <span>21.20.2014</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default BlogCard;
